57f5a7db7130fbb0509182da6799602c95f6c2ab
Haile Lagi <52631736+hailelagi@users.noreply.github.com>
2024-11-18 17:59:27 +0100

    first + drop
    Signed-off-by: Haile Lagi <52631736+hailelagi@users.noreply.github.com>

--- src/first.rs
+++ src/first.rs
@@ -62,52 +62,40 @@ impl List {
         self.head = Link::More(new_node);
     }
 
-    fn pop(&mut self) -> Option<()> {
-        match self.head {
-            Link::Empty => todo!(),
-            Link::More(_) => todo!()
+    fn pop(&mut self) -> Option<i32> {
+        match mem::take(&mut self.head) {
+            Link::Empty => None,
+            Link::More(node) => {
+                self.head = node.next;
+                Some(node.elem)
+            }
         }
     }
 }
 
 #[cfg(test)]
-mod tests {
-    use super::*;
+mod test {
+    use super::List;
 
     #[test]
     fn test_new_list() {
         let list = List::new();
-        assert!(matches!(list, List { head: Link::Empty }));
+        assert!(matches!(list, List { head: _ }));
     }
 
     #[test]
-    fn test_push_list() {
+    fn test_push_n_pop_list() {
         let mut list = List::new();
         list.push(1);
         list.push(2);
         list.push(3);
         list.push(4);
 
-        if let Link::More(node) = &list.head {
-            assert_eq!(node.elem, 4);
-            if let Link::More(node) = &node.next {
-                assert_eq!(node.elem, 3);
-                if let Link::More(node) = &node.next {
-                    assert_eq!(node.elem, 2);
-                    if let Link::More(node) = &node.next {
-                        assert_eq!(node.elem, 1);
-                        assert!(matches!(node.next, Link::Empty));
-                    } else {
-                        panic!("Expected node with elem 1");
-                    }
-                } else {
-                    panic!("Expected node with elem 2");
-                }
-            } else {
-                panic!("Expected node with elem 3");
-            }
-        } else {
-            panic!("Expected node with elem 4");
-        }
+        assert_eq!(list.pop(), Some(4));
+        assert_eq!(list.pop(), Some(3));
+
+        list.push(5);
+
+        assert_eq!(list.pop(), Some(5));
     }
 }
