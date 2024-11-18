0a36d3274db8eb75d8fbe37611f6c9f7961d3928
Haile Lagi <52631736+hailelagi@users.noreply.github.com>
2024-11-18 18:16:53 +0100

    refactor generics
    Signed-off-by: Haile Lagi <52631736+hailelagi@users.noreply.github.com>

--- src/second.rs
+++ src/second.rs
@@ -1,23 +1,23 @@
 //! Linked list
 //!
 
-pub struct List {
-    head: Link,
+pub struct List <T> {
+    head: Link<T>,
 }
 
-type Link = Option<Box<Node>>;
+type Link<T> = Option<Box<Node<T>>>;
 
-struct Node {
-    elem: i32,
-    next: Link,
+struct Node <T> {
+    elem: T,
+    next: Link<T>,
 }
 
-impl List {
+impl<T> List<T> {
     pub fn new() -> Self {
         List { head: None }
     }
 
-    pub fn push(&mut self, elem: i32) {
+    pub fn push(&mut self, elem: T) {
         let new_node = Box::new(Node {
             elem: elem,
             next: self.head.take(),
@@ -26,18 +26,15 @@ impl List {
         self.head = Link::Some(new_node);
     }
 
-    pub fn pop(&mut self) -> Option<i32> {
-        match self.head.take() {
-            Link::None => None,
-            Link::Some(node) => {
-                self.head = node.next;
-                Some(node.elem)
-            }
-        }
+    pub fn pop(&mut self) -> Option<T> {
+        self.head.take().map(|n| {
+            self.head = n.next;
+            n.elem
+        })
     }
 }
 
-impl Drop for List {
+impl <T> Drop for List<T> {
     fn drop(&mut self) {
         let mut cur_link = self.head.take();
 
