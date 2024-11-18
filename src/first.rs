717172742ede1538e70d13df3b003bc3355b6eb7
Haile Lagi <52631736+hailelagi@users.noreply.github.com>
2024-11-18 15:18:44 +0100

    layout
    Signed-off-by: Haile Lagi <52631736+hailelagi@users.noreply.github.com>

--- src/first.rs
+++ src/first.rs
@@ -1,16 +1,38 @@
 // first.rs
 // List a = Empty | Elem a (List a)
 
+/**
+* We're allocating a node that just says "I'm not actually a Node"
+* One of our nodes isn't heap-allocated at all.
+* numa-awareness
 // [node, ptr] -> [node, ptr] -> [node, ptr] -> [node, ptr]
+**/
 
-#[derive(Debug)]
-pub struct Node {
-    pub elem: i32,
-    pub next: List,
-}
-
-#[derive(Debug)]
+/* list.rs
 pub enum List {
     Empty,
+    Elem(Box<List>),
+}
+*/
+
+/*
+pub enum TwoList {
+    Empty,
+    ElemThenEmpty(i32),
+    ElemThenNotEmpty(i32, Box<List>),
+}
+*/
+
+pub struct List {
+    head: Link,
+}
+
+enum Link {
+    Empty,
     More(Box<Node>),
+}
+
+struct Node {
+    elem: i32,
+    next: Link,
 }
